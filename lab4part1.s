d820ed6583e0edc35666fc67074cc3f606b96947
Daniel Colosa <123604619+DPC5@users.noreply.github.com>
2026-02-17 09:55:48 -0500

    Basic
    Made basic push button and LED control, created init and started main

---
+++ lab4part1.s
@@ -0,0 +1,81 @@
+	.text 
+	.global lab4
+
+lab4:
+	PUSH {r4-r12,lr}	; Spill registers to stack
+       
+init_gpio:
+
+    MOV r0, #0xE608
+    MOVT r0, #0x400F ; RCGCGPIO Register Address
+
+    LDR r1, [r0] ; Read the current value of RCGCGPIO
+    ORR r1, r1, #0x20 ; set the bit to enable the clock for port f
+    STR r1, [r0] ; Write back the updated value to RCGCGPIO
+
+wait_gpio:
+    MOV r0, #0xEA08
+    MOVT r0, #0x400F ; RCGCGPIO Register Address
+
+    LDR r1, [r0] ; Read the current value of RCGCGPIO
+    AND r1, r1, #0x20 ; Check if the clock for port f is enabled
+    CMP r1, #0 ; If not enabled, wait
+    BEQ wait_gpio
+
+    ; else keep going
+    MOV r0, #0x5000
+    MOVT r0, #0x4002 ; PORT F Base Address
+
+    MOV r1, #0x0E ; Set the direction of the pins (PF1-4 as output)
+    STR r1, [r0, #0x400] ; Write to the GPIODIR register
+
+    ; digital
+    MOV r1, #0x1E ; enable PF1-4
+    STR r1, [r0, #0x51C]
+
+    MOV r1, #0x10 ; enable pull-up resistor for PF4
+    STR r1, [r0, #0x510]
+
+main_loop:
+    BL read_tiva_push_button ; Read the state of the push button  
+ 
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+
+
+illuminate_RGB_LED: 
+	PUSH {r4-r12,lr}	; Spill registers to stack
+    
+    MOV r1, #0x5000
+    MOVT r1, #0x4002 ; PORT F Base Address
+
+    STRB r0, [r1, #0x3FC] ; Write the value to the LED pins (PF1-3)
+
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+
+read_tiva_push_button: 
+	PUSH {r4-r12,lr}	; Spill registers to stack
+    
+    ; r0 = 1 if pressed, 0 if not pressed
+
+    MOV r1, #0x5000
+    MOVT r1, #0x4002 ; PORT F Base Address
+
+    LDRB  r2, [r1, #0x3FC] ; Read the value of the push buttons
+    AND r2, r2, #0x10 ; Mask the bits except for SW1
+
+    CMP r2, #0 ; check if pressed LOW
+    BEQ sw1_pressed
+
+    MOV r0, #0 ; SW1 not pressed
+    B sw1_end
+ 
+sw1_pressed:
+    MOV r0, #1 ; SW1 pressed
+
+sw1_end:
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+
+	.end
